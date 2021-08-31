06dca6c71ed7a0cab39caf7b46476e114cdbf897
Ducakdw <kdw15082@gmail.com>
2021-08-31 09:38:45 +0900

    인증페이지 링크 연동

--- fintech/src/pages/AddUser.js
+++ fintech/src/pages/AddUser.js
@@ -6,7 +6,7 @@ const AddUser = () => {
     const openAuthSite = () =>{
         let tmpwindow = window.open("about:blank");
         let clinet_id = "85dd2bdc-69d2-4b43-b75f-2886dbf2a027"
-        tmpwindow.location.href = "https://naver.com";
+        tmpwindow.location.href = `https://testapi.openbanking.or.kr/oauth/2.0/authorize?response_type=code&client_id=${clinet_id}&redirect_uri=http://localhost:3000/authResult&scope=login inquiry transfer&state=12345678901234567890123456789012&auth_type=0`;
     }
 
     return (
