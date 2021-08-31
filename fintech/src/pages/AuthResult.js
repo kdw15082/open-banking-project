5db7f22a8838739fcf16cc3886cbd03997b407e7
Ducakdw <kdw15082@gmail.com>
2021-08-31 09:54:32 +0900

    인증코드 화면 출력 쿼리스트링

--- fintech/src/pages/AuthResult.js
+++ fintech/src/pages/AuthResult.js
@@ -1,11 +1,17 @@
-import React from 'react'
-import Header from '../component/Header'
+import React from 'react';
+import Header from '../component/Header';
+import {useLocation} from "react-router-dom";
+import queryString from "query-string";
 
 
 const AuthResult = () => {
+    const {search} = useLocation();
+    const {code} = queryString.parse(search);
+
     return (
         <div>
             <Header title="인증 결과"></Header>
+            <p>인증 코드 : {code}</p>
         </div>
     )
 }
