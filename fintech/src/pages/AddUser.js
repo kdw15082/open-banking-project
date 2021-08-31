15841640bb48ac5574a6fa4849d0d64537746785
Ducakdw <kdw15082@gmail.com>
2021-08-31 09:30:48 +0900

    인증사이트 열기 버튼 연동

--- fintech/src/pages/AddUser.js
+++ fintech/src/pages/AddUser.js
@@ -2,9 +2,17 @@ import React from 'react'
 import Header from '../component/Header'
 
 const AddUser = () => {
+
+    const openAuthSite = () =>{
+        let tmpwindow = window.open("about:blank");
+        let clinet_id = "85dd2bdc-69d2-4b43-b75f-2886dbf2a027"
+        tmpwindow.location.href = "https://naver.com";
+    }
+
     return (
         <div>
             <Header title="사용자 추가"></Header>
+            <button onClick={openAuthSite}>사용자 인증</button>
         </div>
     )
 }
