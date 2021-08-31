3dd189d4953216756ac2e3f5b24f4b05862a8efb
Ducakdw <kdw15082@gmail.com>
2021-08-31 10:38:02 +0900

    Axios 를 통한 토큰 요청 완성

--- fintech/src/pages/AuthResult.js
+++ fintech/src/pages/AuthResult.js
@@ -2,7 +2,7 @@ import React ,{useState, useEffect} from 'react';
 import Header from '../component/Header';
 import {useLocation} from "react-router-dom";
 import queryString from "query-string";
-
+import axios from 'axios';
 
 const AuthResult = () => {
     const {search} = useLocation();
@@ -15,6 +15,29 @@ const AuthResult = () => {
 
     const getAccessToken = () =>{
         console.log("토큰을 가져옵니다");
+        const sendData = {
+            code : code,
+            client_id:"85dd2bdc-69d2-4b43-b75f-2886dbf2a027",
+            client_secret: "45fec24c-4ddb-489d-ac2b-072cb5d23c21",
+            redirect_uri: "http://localhost:3000/authResult",
+            grant_type: "authorization_code",
+        };
+
+        const encodedSendDdata = queryString.stringify(sendData);
+
+        const option = {
+            method: "POST",
+            url : "https://testapi.openbanking.or.kr/oauth/2.0/token",
+            headers:{
+                "Content-Type":"application/x-www-form-urlencoded"
+            },
+            data : encodedSendDdata,
+        };
+
+        axios(option).then((response) =>{
+            console.log(response);
+        })
+
     };
 
     return (
