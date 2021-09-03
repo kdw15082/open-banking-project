134ad8af2b5fd39b47fa7b30e5d820bfeeb43ad2
Ducakdw <kdw15082@gmail.com>
2021-09-03 22:22:08 +0900

    사용자 계좌 목록 불러오기

--- fintech/src/pages/Main.js
+++ fintech/src/pages/Main.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, {useEffect, useState } from 'react'
 import Header from '../component/Header';
+import axios from "axios";
 
 const Main = () => {
+    const [accoountList, setAccountList] = useState([]);
+
+
+    useEffect(() => {
+        getAccountList();
+    },[]);
+
+    const getAccountList  = () => {
+        const accessToken = localStorage.getItem("accessToken");
+        const userSeqNo = localStorage.getItem("userSeqNo");
+
+    }
+
     return (
         <div>
             <Header title={"메인"}></Header>
