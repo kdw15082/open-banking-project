2799907f028f872332cfbe87026f7acca414632e
Ducakdw <kdw15082@gmail.com>
2021-09-04 13:22:10 +0900

    메인 리스트 카드 컴포넌트

--- fintech/src/pages/Main.js
+++ fintech/src/pages/Main.js
@@ -27,12 +27,14 @@ const Main = () => {
 
         axios(option).then(({data}) =>{
             console.log(data);
+            setAccountList(data.res_list);
         });
     }
 
     return (
         <div>
             <Header title={"메인"}></Header>
+            {accoountList.map((account) =>{})}
         </div>
     )
 }
